5a1236556d6b35a543f93db612f847c0159cc646
GabrielRossi <gabrieldesouzarossi123456@gmail.com>
2023-02-22 18:14:09 -0300

    22/02/2023

---
+++ js_1/script.js
@@ -0,0 +1,18 @@
+
+
+let rossi = {
+    name: 'rossi',
+    age: 19, 
+    subscribe: false,
+}
+
+const jhon = {
+    name: 'jhon',
+    age: 20,
+    subscribe: true,
+}
+
+
+let studants = [rossi];
+studants[1] = jhon
+console.log(studants)
